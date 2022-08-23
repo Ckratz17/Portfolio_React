3f855255757e8cfce9f9c5ae53562e85c3e4d31f
Chris Kratz <Ckratz17@gmail.com>
2022-08-23 19:06:33 -0400

    added pages folder and set up the layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,6 @@
-import React, { Component } from 'react';
-import About from './components/About'
-import Header from './components/Header'
-import Portfolio from './components/Portfolio'
-import Resume from './components/Resume'
-import Footer from './components/Footer'
-import Contact from './components/Contact'
+import React from 'react';
+import PortfolioContainer from "./components/PortfolioContainer"
 
-class App extends Component {
-    
-//SET UP RETURN
-    render(){
-      return (
-        <div className='App'>
-            <Header />
-            <About />
-            <Contact />
-            <Resume />
-            <Portfolio />
-            <Footer />
-        </div>
-      )
-    }  
-}
+const App= () => <PortfolioContainer />
 
 export default App
