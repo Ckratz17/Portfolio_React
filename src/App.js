1072c3511fa91c85c9adb82727faba5f700ee421
Chris Kratz <Ckratz17@gmail.com>
2022-08-22 23:40:41 -0400

    got the page to render successfully and added additional files

--- src/App.js
+++ src/App.js
@@ -1,33 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import About from './components/About'
 import Header from './components/Header'
-import Nav from './components/Nav'
-import Page from './components/Page'
+import Portfolio from './components/Portfolio'
+import Resume from './components/Resume'
 import Footer from './components/Footer'
+import Contact from './components/Contact'
 
-function App() {
-    const [pages] = useState([
-        { name: 'about me' },
-    ])
-
-
-// set up useState with currentPage and setCurrentPage giving the default state to be the landing page
-
-// example - pages[0] would show the about me page first
-
+class App extends Component {
+    
 //SET UP RETURN
-return (
-    //container
-        //header component wrapping the Nav 
-        //Nav- give the Nav component some props- pages={pages},
-        //setCurrentPage={setCurrentPage}, currentPage={currentPage}
-        //close the header
-
-        //main tag wrapping the page component
-            //Page component with the prop of currentPage={currentPage}
-            //close main
-        //footer component
-        //close container
-    )
+    render(){
+      return (
+        <div className='App'>
+            <Header />
+            <About />
+            <Contact />
+            <Resume />
+            <Portfolio />
+            <Footer />
+        </div>
+      )
+    }  
 }
 
 export default App
